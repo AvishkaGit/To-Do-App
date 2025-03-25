12f42d49d7a35b9ee36c7549c668e92b1cf5dc77
Avishka madhuranga <131765154+AvishkaGit@users.noreply.github.com>
2025-03-25 23:06:41 +0530

    haf done
    map function

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [todos, setTodos] = useState([
+    { id: 1, todo: 'Learn React' }
+  ]);
+  const [input, setInput] = useState('');
+
+  const addTodo = async () => {
+    try {
+      if (input.trim() !== '') {
+        setTodos([...todos, { id: new Date(), todo: input }])
+        setInput('')
+      }
+
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
 
 
   return (
@@ -14,21 +32,25 @@ function App() {
             type="text"
             placeholder='Add a Todo'
             className='py-2 px-4 border rounded w-full focus:outline-none mr-2'
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
           />
 
-          <button className='bg-gradient-to-r from-blue-400 to-blue-600 text-white py-2 px-4 rounded'>Add</button>
+          <button onClick={addTodo} className='bg-gradient-to-r from-blue-400 to-blue-600 text-white py-2 px-4 rounded'>Add</button>
 
         </div>
       </div>
       <div className='bg-gray-100 p-6 rounded shadow-md w-full max-w-lg lg:w-1/4 '>
         <ul>
-          <li className='flex items-center justify-between bg-white p-3 rounded shadow-md mb-3'>
-            <span className='text-lg'>Learn React</span>
-            <div>
-              <button className='mr-2 p-2 bg-gradient-to-r from-gray-400 to-gray-600 text-white rounded hover:from-gray-500 hover:to-gray-700'>Edit</button>
-              <button className='mr-2 p-2 bg-gradient-to-r from-red-400 to-red-600 text-white rounded hover:from-red-500 hover:to-red-700'>Remove</button>
-            </div>
-          </li>       
+          {todos.map((todo, index) => {
+            <li key={index} className='flex items-center justify-between bg-white p-3 rounded shadow-md mb-3'>
+              <span className='text-lg'>{todos.todo}</span>
+              <div>
+                <button className='mr-2 p-2 bg-gradient-to-r from-gray-400 to-gray-600 text-white rounded hover:from-gray-500 hover:to-gray-700'>Edit</button>
+                <button className='mr-2 p-2 bg-gradient-to-r from-red-400 to-red-600 text-white rounded hover:from-red-500 hover:to-red-700'>Remove</button>
+              </div>
+            </li>
+          })}
         </ul>
       </div>
     </div>
